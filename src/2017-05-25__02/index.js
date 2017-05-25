50de57b835d087f95b8124288fb673ddfd708a2c
Peter Krevenets <peter.k.nets@gmail.com>
2017-05-25 11:24:03 +0300

    Use splice in removeCounter

--- src/2017-05-25__02/index.js
+++ src/2017-05-25__02/index.js
@@ -1,5 +1,10 @@
+const removeCounter = (list, index) => {
+  list.splice(index, 1);
+  return list;
+};
+
 const addCounter = (list) => {
-  return list.concat([0]);
+  return [...list, 0];
 };
 
 const testAddCounter = () => {
@@ -13,5 +18,15 @@ const testAddCounter = () => {
   ).toEqual(listAfter);
 };
 
+const testRemoveCounter = () => {
+  const listBefore = [0, 10, 20];
+  const listAfter = [0, 20];
+
+  expect(
+    removeCounter(listBefore, 1)
+  ).toEqual(listAfter);
+};
+
 testAddCounter();
+testRemoveCounter();
 console.log('All tests passed.');
