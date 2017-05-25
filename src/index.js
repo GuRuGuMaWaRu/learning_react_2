38d6a7b2c2d110635ba3760aec5337a3c6834c19
Peter Krevenets <peter.k.nets@gmail.com>
2017-05-25 10:39:56 +0300

    React counter example

--- src/index.js
+++ src/index.js
@@ -13,22 +13,38 @@ const counter = (state = 0, action) => {
   }
 }
 
-const Counter = ({ value }) => (
-  <h1>{value}</h1>
+const Counter = ({
+  value,
+  onIncrement,
+  onDecrement
+ }) => (
+  <div>
+    <h1>{value}</h1>
+    <button onClick={onIncrement}>+</button>
+    <button onClick={onDecrement}>-</button>
+  </div>
 );
 
 const store = createStore(counter);
 
 const render = () => {
   ReactDOM.render(
-    <Counter value={store.getState()} />,
+    <Counter
+      value={store.getState()}
+      onIncrement={() =>
+        store.dispatch({
+          type: 'INCREMENT'
+        })
+      }
+      onDecrement={() =>
+        store.dispatch({
+          type: 'DECREMENT'
+        })
+      }
+    />,
     document.getElementById('root')
   );
 };
 
 store.subscribe(render); // subscribe to any state changes
 render(); // render initial state
-
-document.addEventListener('click', () => {
-  store.dispatch({ type: 'INCREMENT' }); // 2
-});
